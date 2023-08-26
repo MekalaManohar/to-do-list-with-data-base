4154d5bedb8cd53ab9893c5ae5c10716b1bfa349
Manohar-Letsflo <manohar.m@letsflo.co>
2023-08-26 20:09:38 +0530

    Update

--- t.js
+++
@@ -1,7 +0,0 @@
-const originalDate = "2023-08-26";
-const formattedDate = new Date(originalDate).toLocaleDateString("en-IN", {
-  day: "2-digit",
-  month: "2-digit",
-  year: "numeric"
-});
-console.log(formattedDate); // 08/26/2023
